d4b511ac06bc0f9c33d6d6c49119f491a419fe98
christophe <christophegueroult@gmail.com>
2021-08-30 17:13:34 +0200

    pages application components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { LoginModule } from './login/login.module';
-import { TemplateModule } from './template/template.module';
-import { UiModule } from './ui/ui.module';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
