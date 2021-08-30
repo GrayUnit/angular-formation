999b11c122779861246d41deaba7dcbbecf5be17
christophe <christophegueroult@gmail.com>
2021-08-30 15:49:20 +0200

    modules app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('crm-betclic app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('crm-betclic');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'crm-betclic';
+  public title = 'crm-betclic';
 }
