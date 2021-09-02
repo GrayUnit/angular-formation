d58dbadbc815b3f30ad100cbad8454f22bfa37f3
christophe <christophegueroult@gmail.com>
2021-09-02 15:35:49 +0200

    LOCAL ID and DEFAULT CURRENCY CODE

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
