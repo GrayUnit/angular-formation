35fb69b6327c6930623fa0fc871b63b372273997
christophe <christophegueroult@gmail.com>
2021-09-08 18:14:43 +0200

    TP CLIENT (PAGE LIST CLIENTS AND ROUTES)

--- src/app/clients/pages/page-list-clients/page-list-clients.component.ts
+++ src/app/clients/pages/page-list-clients/page-list-clients.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { StateClient } from 'src/app/core/enums/state-client';
+import { Client } from 'src/app/core/models/client';
+import { ColClientsService } from 'src/app/core/services/col-clients.service';
 
 @Component({
   selector: 'app-page-list-clients',
@@ -6,7 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page-list-clients.component.scss'],
 })
 export class PageListClientsComponent implements OnInit {
-  constructor() {}
+  public collection$: Subject<Client[]>;
+  public states = Object.values(StateClient);
+  // private sub!: Subscription;
+  public titre = 'List Clients';
+  // public collection!: Order[];
+  public entetes = [
+    'Action',
+    'Name',
+    'Email',
+    'Total CA HT',
+    'Taux TVA',
+    'Total CA TTC',
+    'State',
+  ];
+  constructor(
+    private clientsService: ColClientsService,
+    private router: Router
+  ) {
+    this.collection$ = this.clientsService.collection;
+  }
 
   ngOnInit(): void {}
+
+  public changeState(item: Client, event: any): void {
+    const state = event.target.value;
+    this.clientsService.changeState(item, state).subscribe((data) => {
+      Object.assign(item, data);
+    });
+  }
+
+  public goToEdit(id: number): void {
+    this.router.navigate(['list-clients', 'edit-client', id]);
+  }
+  public deleteItem(id: number): void {
+    this.clientsService.delete(id).subscribe();
+  }
 }
