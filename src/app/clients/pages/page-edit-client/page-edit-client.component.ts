0ed71f6d9fc0fc042dbec6193edd7d46f01b6ade
christophe <christophegueroult@gmail.com>
2021-09-08 18:31:52 +0200

    TP PAGE EDIT CLIENT (PAGE EDIT CLIENT COMPONENT)

--- src/app/clients/pages/page-edit-client/page-edit-client.component.ts
+++ src/app/clients/pages/page-edit-client/page-edit-client.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Client } from 'src/app/core/models/client';
+import { ColClientsService } from 'src/app/core/services/col-clients.service';
 
 @Component({
   selector: 'app-page-edit-client',
   templateUrl: './page-edit-client.component.html',
-  styleUrls: ['./page-edit-client.component.scss']
+  styleUrls: ['./page-edit-client.component.scss'],
 })
 export class PageEditClientComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  public item$!: Observable<Client>;
+  constructor(
+    private route: ActivatedRoute,
+    private clientsService: ColClientsService,
+    private router: Router
+  ) {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.item$ = this.clientsService.getItemById(id);
+    });
   }
 
+  ngOnInit(): void {}
+
+  public update(item: Client): void {
+    this.clientsService.update(item).subscribe((res) => {
+      this.router.navigate(['list-clients']);
+    });
+  }
 }
