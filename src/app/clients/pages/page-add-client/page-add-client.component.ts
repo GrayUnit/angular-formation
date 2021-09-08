5155f644ef8de09ff967fbc7f07e87915328a702
christophe <christophegueroult@gmail.com>
2021-09-08 18:28:22 +0200

    TP PAGE ADD CLIENT (PAGE ADD CLIENT COMPONENT)

--- src/app/clients/pages/page-add-client/page-add-client.component.ts
+++ src/app/clients/pages/page-add-client/page-add-client.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Client } from 'src/app/core/models/client';
+import { ColClientsService } from 'src/app/core/services/col-clients.service';
 
 @Component({
   selector: 'app-page-add-client',
   templateUrl: './page-add-client.component.html',
-  styleUrls: ['./page-add-client.component.scss']
+  styleUrls: ['./page-add-client.component.scss'],
 })
 export class PageAddClientComponent implements OnInit {
+  constructor(
+    private clientsService: ColClientsService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  public add(item: Client): void {
+    this.clientsService.add(item).subscribe((res) => {
+      this.router.navigate(['list-clients']);
+    });
   }
-
 }
