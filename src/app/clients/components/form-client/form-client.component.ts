cb781325a72581ddc6813ec29036b6b076e67f15
christophe <christophegueroult@gmail.com>
2021-09-08 18:22:57 +0200

    TP PAGE ADD CLIENT (FORM CLIENT COMPONENT)

---
+++ src/app/clients/components/form-client/form-client.component.ts
@@ -0,0 +1,33 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { StateClient } from 'src/app/core/enums/state-client';
+import { Client } from 'src/app/core/models/client';
+
+@Component({
+  selector: 'app-form-client',
+  templateUrl: './form-client.component.html',
+  styleUrls: ['./form-client.component.scss'],
+})
+export class FormClientComponent implements OnInit {
+  public form!: FormGroup;
+  public states = Object.values(StateClient);
+  @Input() init = new Client();
+  @Output() submited = new EventEmitter<Client>();
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      id: [this.init.id],
+      name: [this.init.name],
+      email: [this.init.email],
+      total_ca_ht: [this.init.total_ca_ht],
+      taux_tva: [this.init.taux_tva],
+      state: [this.init.state],
+    });
+  }
+
+  public onSubmit() {
+    this.submited.emit(this.form.value);
+  }
+}
